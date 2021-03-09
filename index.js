e7f7b8ddcf172177307dbe32bbf8413fa45b2a8e
Michael Chan <mijoch@gmail.com>
2021-03-09 14:54:52 -0800

    add lesson one draft

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+// About this course:
+// This course is a reference course.
+// I want to expose you to everything you can do with modules before diving into how to utilize them with React.
+
+// lesson 1: modules leak
+// * global code is global code, even if in a module
+// * this is useful for setup in a global environment like with styling in a React project.
+
 import "./lesson/1-modules-leak.mjs";
+console.log(global.test);
