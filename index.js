7f68f0c0c490aba73d3c0824ec51694ded1ba1f9
Michael Chan <mijoch@gmail.com>
2021-03-09 15:03:49 -0800

    add lesson 2 draft

--- index.js
+++ index.js
@@ -6,5 +6,21 @@
 // * global code is global code, even if in a module
 // * this is useful for setup in a global environment like with styling in a React project.
 
-import "./lesson/1-modules-leak.mjs";
-console.log(global.test);
+// import "./lesson/1-modules-leak.mjs";
+// console.log(global.test);
+
+// lesson 2: modules can be imported asyncronously
+// * how will the output change if we change this to a dynamic import?
+// * how can we fix it? with .then()
+// * we can alse use await
+// * and add conditionals
+
+// import("./lesson/1-modules-leak.mjs").then(() => console.log(global.test));
+
+const env = "production";
+
+if (env === "development") {
+  await import("./lesson/1-modules-leak.mjs");
+  console.log(global.test);
+  console.log("hey! we're in DEV. let's party!");
+}
